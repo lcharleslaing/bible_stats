abac59f05238f6055b75dd13d0cd2b85c91f45c7
Lee Charles Laing <71520336+lcharleslaing@users.noreply.github.com>
2023-11-18 18:04:38 -0500

    Update Navbar links

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,7 +4,7 @@
 <div class="mx-2 my-4 flex justify-center gap-1 items-center">
   <a class="btn btn-xs btn-success" href="/">Verses </a>
   <a disabled class="btn btn-xs btn-primary" href="/dates">Dates </a>
-  <a class="btn btn-xs btn-success" href="/quantities">Quantities </a>
-  <a class="btn btn-xs btn-success" href="/sequences">Sqeuences </a>
+  <a class="btn btn-xs btn-success" href="/quantities">Numbers </a>
+  <a class="btn btn-xs btn-success" href="/sequences">Times Used </a>
   <a disabled class="btn btn-xs btn-primary" href="/patterns">Patterns </a>
 </div>
