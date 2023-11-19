acd33f2452e6472ceb469eb468b2d41bd01442d8
Lee Charles Laing <71520336+lcharleslaing@users.noreply.github.com>
2023-11-19 15:55:02 -0500

    Add search functionality to pattern page

--- src/routes/patterns/bybook/+page.svelte
+++ src/routes/patterns/bybook/+page.svelte
@@ -3,6 +3,50 @@
   import bibleSequences from "$lib/data/bible_sequences.json";
   import TitleBanner from "$lib/components/TitleBanner.svelte";
 
+  let searchPattern = "";
+  let filteredPatterns = {};
+
+  // Reactive statement to update filtered patterns
+  $: {
+    const searchNumbers = searchPattern
+      .trim()
+      .split(" ")
+      .map(Number)
+      .filter((n) => !isNaN(n));
+    console.log("Search Numbers:", searchNumbers); // Debugging
+    filteredPatterns =
+      searchPattern.trim() === ""
+        ? aggregatePatterns(bibleSequences)
+        : filterPatterns(searchNumbers);
+  }
+
+  function startsWithSequence(patternString, sequence) {
+    const pattern = patternString.split(",").map(Number);
+    if (sequence.length > pattern.length) return false;
+    for (let i = 0; i < sequence.length; i++) {
+      if (pattern[i] !== sequence[i]) return false;
+    }
+    return true;
+  }
+
+  function filterPatterns(searchNumbers) {
+    const filtered = {};
+    Object.entries(aggregatePatterns(bibleSequences)).forEach(
+      ([count, books]) => {
+        Object.entries(books).forEach(([book, patterns]) => {
+          const matchingPatterns = patterns.filter((patternString) =>
+            startsWithSequence(patternString, searchNumbers)
+          );
+          if (matchingPatterns.length > 0) {
+            if (!filtered[count]) filtered[count] = {};
+            filtered[count][book] = matchingPatterns;
+          }
+        });
+      }
+    );
+    return filtered;
+  }
+
   // Function to aggregate patterns by count and then by book
   function aggregatePatterns(bibleData) {
     const groupedByCount = {};
@@ -49,19 +93,30 @@
   thirdTitle="It's Praise JESUS Let's Gooooo"
 />
 
+<!-- Search Input with Clear Button -->
+<div class="text-center my-4 flex justify-center items-center gap-2">
+  <input
+    type="text"
+    class="input input-bordered"
+    placeholder="Enter patterns (e.g., 1 3 7)"
+    bind:value={searchPattern}
+  />
+  <button class="btn btn-error" on:click={() => (searchPattern = "")}>
+    Clear
+  </button>
+</div>
+
 <!-- Display Grouped Patterns by Count, then by Book -->
 <div class="p-4">
-  <h1 class="text-center text-lg text-center">
+  <h1 class="text-center text-lg">
     Click the <strong>Buttons</strong> below to open the
     <strong>Section</strong>
     to view the <strong>Books</strong> within each
     <strong>Count</strong>
   </h1>
-  {#each Object.entries(groupedPatterns) as [count, books]}
+  {#each Object.entries(filteredPatterns) as [count, books]}
     <div class="my-2">
       <!-- Count Accordion Toggle -->
-      <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
       <h2
         class="text-4xl p-2 bg-success text-slate-900 text-center rounded-lg font-bold my-4 cursor-pointer"
         on:click={() => toggleCount(count)}
@@ -73,8 +128,6 @@
         {#each Object.entries(books) as [book, patterns]}
           <div class="my-2">
             <!-- Book Accordion Toggle -->
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <!-- svelte-ignore a11y-no-static-element-interactions -->
             <div
               class="flex card p-2 bg-primary text-slate-900 rounded-lg mb-4 cursor-pointer"
               on:click={() => toggleBook(book)}
