6cc1a601936bdf26e553e25dfd2e9398e97f27cb
Lee Charles Laing <71520336+lcharleslaing@users.noreply.github.com>
2023-11-19 21:45:57 -0500

    Refactor date filtering and grouping

--- src/routes/dates/+page.svelte
+++ src/routes/dates/+page.svelte
@@ -4,117 +4,144 @@
   import TitleBanner from "$lib/components/TitleBanner.svelte";
   import { bibleData, fetchBibleData } from "$lib/bibleDataStore";
 
-  // Declare and initialize variables
   let searchNumber = "";
+  let searchKeyword = ""; // Store the current keyword
   let allVerses = [];
-  let versesWithKeywords = [];
-  let filteredVerses = [];
+  let groupedVerses = {};
+  let openBook = null;
 
   onMount(() => {
-    fetchBibleData(); // Fetch the data when the component mounts
+    fetchBibleData();
   });
+
+  // Singular keywords (without plurals) from the JSON file
+  let keywords = dateKeywords.keywords;
 
   $: {
     if ($bibleData && $bibleData.length > 0) {
       allVerses = $bibleData.flatMap((book) => book.verses_with_numbers);
-      versesWithKeywords = filterVersesByKeywords(
-        dateKeywords.keywords,
+      groupedVerses = groupAndFilterVerses(
+        searchNumber,
+        searchKeyword,
         allVerses
       );
-
-      const searchNumbers = searchNumber
-        .trim()
-        .split(" ")
-        .map(Number)
-        .filter((n) => !isNaN(n));
-      filteredVerses = searchNumber.trim()
-        ? filterAndModifyVerses(
-            searchNumbers,
-            dateKeywords.keywords,
-            versesWithKeywords
-          )
-        : versesWithKeywords;
+      openBook = null; // Close all accordions when filter changes
     } else {
       allVerses = [];
-      versesWithKeywords = [];
-      filteredVerses = [];
+      groupedVerses = {};
+      openBook = null; // Close all accordions if there are no data
     }
   }
 
-  function filterVersesByKeywords(keywords, verses) {
-    return verses.filter((verse) =>
-      keywords.some((keyword) => verse.text.includes(keyword))
+  function groupVerses(verses) {
+    return verses.reduce((acc, verse) => {
+      if (!acc[verse.book]) acc[verse.book] = [];
+      acc[verse.book].push(verse);
+      return acc;
+    }, {});
+  }
+
+  function groupAndFilterVerses(number, keyword, verses) {
+    let filtered = verses.filter((verse) => {
+      const numberMatch = number.trim() ? verse.text.includes(number) : true;
+      const keywordMatch = keyword.trim() ? verse.text.includes(keyword) : true;
+      return numberMatch && keywordMatch;
+    });
+    return groupVerses(filtered);
+  }
+
+  function setKeywordFilter(keyword) {
+    searchKeyword = keyword;
+    groupedVerses = groupAndFilterVerses(
+      searchNumber,
+      searchKeyword,
+      allVerses
+    );
+    openBook = null; // Close all accordions when filter changes
+  }
+
+  function filterVersesByKeyword(keyword, verses) {
+    // Search for both singular and plural forms of the keyword
+    const pluralKeyword = keyword + "s"; // Assuming plural is just 's' added
+    return verses.filter(
+      (verse) =>
+        verse.text.includes(keyword) || verse.text.includes(pluralKeyword)
     );
   }
 
-  function filterAndModifyVerses(numbers, keywords, verses) {
-    return verses
-      .map((verse) => {
-        let modifiedText = verse.text;
-        numbers.forEach((number) => {
-          keywords.forEach((keyword) => {
-            const regex = new RegExp(`\\b${number}(\\s+${keyword})\\b`, "gi");
-            modifiedText = modifiedText.replace(
-              regex,
-              (match, keywordMatch) => {
-                return `${number}<strong>${keywordMatch.toUpperCase()}</strong>`;
-              }
-            );
-          });
-        });
-        return modifiedText !== verse.text
-          ? { ...verse, text: modifiedText }
-          : verse;
-      })
-      .filter((verse) =>
-        numbers.some((number) => verse.text.includes(number.toString()))
-      );
+  function clearSearch() {
+    searchNumber = "";
+    searchKeyword = "";
+    openBook = null; // Close all accordions on clear
+  }
+
+  function toggleBook(book) {
+    openBook = openBook === book ? null : book; // Toggle the open book
   }
 </script>
 
-<!-- UI for displaying filteredVerses and the search input -->
-<div class="my-4">
+<div class="m-4">
   <TitleBanner
     secondTitle="Dates in Bible Verses"
     thirdTitle="It's Praise JESUS Let's Gooooo"
   />
-</div>
 
-<div class="m-4">
+  <!-- Keyword Filter Buttons -->
+  <p class="text-lg font-bold text-green-700">Filter Buttons...</p>
+  <div class="flex flex-wrap gap-2 mb-4">
+    {#each keywords as keyword}
+      <button
+        class={`btn ${
+          searchKeyword === keyword ? "btn-success" : "btn-primary"
+        }`}
+        on:click={() => setKeywordFilter(keyword)}
+      >
+        {keyword}(s)
+      </button>
+    {/each}
+  </div>
+
+  <!-- Search and Clear -->
+  <p class="text-lg font-bold text-green-700">Refine by Adding a Number...</p>
+
   <div class="flex gap-2">
     <input
       class="w-full p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-      id="searchNumber"
       type="text"
       placeholder="Enter a number to search within date verses..."
       bind:value={searchNumber}
     />
-
-    <!-- Clear Button -->
-    <button
-      class="btn btn-error p-2 rounded-lg"
-      on:click={() => (searchNumber = "")}
-    >
+    <button class="btn btn-error p-2 rounded-lg" on:click={clearSearch}>
       Clear
     </button>
   </div>
 
-  <!-- Displaying the total number of verses -->
-  <p class="card bg-slate-200 flex justify-between items-center text-xl my-4">
-    <span class="float-left text-slate-900 font-bold">Date Verses Count:</span>
-    <span
-      class="float-right card p-4 mb-2 bg-slate-700 shadow-lg text-slate-100 font-bold"
-      >{filteredVerses.length}</span
-    >
-  </p>
+  <!-- Displaying Grouped Verses with Accordions and Counts -->
+  {#each Object.entries(groupedVerses) as [book, verses]}
+    <div class="my-4">
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+      <h2
+        class="rounded-lg p-2 font-extrabold bg-success text-slate-900 rouned-lg text-xl text-center font-bold cursor-pointer"
+        on:click={() => toggleBook(book)}
+      >
+        {book}
 
-  <div class="">
-    {#each filteredVerses as verse}
-      <div class="my-2 card bg-slate-100 text-slate-900 p-2">
-        <strong>{verse.book} {verse.chapter}:{verse.verse}</strong>
-        <p>{@html verse.text}</p>
-        <!-- Render modified text as HTML -->
-      </div>
-    {/each}
-  </div>
+        <span
+          class="pb-1 font-normal px-2 pt-0 bg-green-900 text-slate-100 shadow-md rounded-lg"
+          >({verses.length}) Verses</span
+        >
+      </h2>
+      {#if openBook === book}
+        <div class="">
+          {#each verses as verse}
+            <div class="my-2 card bg-slate-100 text-slate-900 p-2">
+              <strong>{verse.book} {verse.chapter}:{verse.verse}</strong>
+              <p>{@html verse.text}</p>
+            </div>
+          {/each}
+        </div>
+      {/if}
+    </div>
+  {/each}
 </div>
